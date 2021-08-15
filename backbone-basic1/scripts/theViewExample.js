e33a105c1a68d22c5b62614559c8c59ebbcacff3
Volodymyr <wolodymyrs@gmail.com>
2021-08-15 22:17:52 +0300

    add dom-el on a fly

--- backbone-basic1/scripts/theViewExample.js
+++ backbone-basic1/scripts/theViewExample.js
@@ -1,13 +1,21 @@
 TheView = Backbone.View.extend({
   defaults: {},
+
+  // to create DOM-obj(or el) on a fly
+  tagName: 'span', // this is html-tag like 'div, p, span ...'
+  className: 'prettyCake', // class we put into html
+  id: 'span1', //
+
   initialize: function () {
     console.log('I Exist now mwa, ha, ha, ha');
     console.log(this.el);
     console.log(this.$el);
-    this.$el.append('Mwa, ha, ha, ha !');
+    $('#unique').append(this.el);
+    // this.$el.append('Mwa, ha, ha, ha !');
   },
 });
 
 $(document).ready(function () {
-  var aView = new TheView({ el: $('#unique') });
+  // var aView = new TheView({ el: $('#unique') });
+  var bView = new TheView();
 });
