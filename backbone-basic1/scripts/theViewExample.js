d86c1b82836e5a0ec415537b422a1a5b686a20be
Volodymyr <wolodymyrs@gmail.com>
2021-08-16 07:03:39 +0300

    template and render function

--- backbone-basic1/scripts/theViewExample.js
+++ backbone-basic1/scripts/theViewExample.js
@@ -1,21 +1,16 @@
 TheView = Backbone.View.extend({
   defaults: {},
 
-  // to create DOM-obj(or el) on a fly
-  tagName: 'span', // this is html-tag like 'div, p, span ...'
-  className: 'prettyCake', // class we put into html
-  id: 'span1', //
-
   initialize: function () {
-    console.log('I Exist now mwa, ha, ha, ha');
-    console.log(this.el);
-    console.log(this.$el);
-    $('#unique').append(this.el);
-    // this.$el.append('Mwa, ha, ha, ha !');
+    this.render();
+  },
+
+  render: function () {
+    var template = _.template($('#nyTemplateOne').html(), {});
+    this.$el.html(template);
   },
 });
 
 $(document).ready(function () {
-  // var aView = new TheView({ el: $('#unique') });
-  var bView = new TheView();
+  var cView = new TheView({ el: $('#unique-two') });
 });
